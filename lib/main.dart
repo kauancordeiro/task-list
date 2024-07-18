7f252d5a5365b08d0a28b727d6df0dba72dc48c3
Kauan <cvitorcordeiro@gmail.com>
2024-07-17 21:54:50 -0300

    txt field and button

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
-
-void main(){
+void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
@@ -19,35 +18,59 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   List _toDoList = [];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de tarefas"),
+        backgroundColor: Colors.blue,
+        elevation: 4,
+        shadowColor: Colors.blueAccent,
+        centerTitle: true,
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(17, 1.0, 7, 1),
+            child: Row(
+              children: [
+                Expanded(child:TextField(
+                  decoration: InputDecoration(
+                      labelText: "Nova Tarefa",
+                      labelStyle: TextStyle(color: Colors.blueAccent)),
+                )),
+                ElevatedButton(
+                  onPressed: (){},
+                  child: Text("ADD"),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 
-  Future<File> _getFile() async{
+  Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
     return File("${directory.path}/data.json");
   }
 
-  Future<File> _saveData() async{
+  Future<File> _saveData() async {
     String data = json.encode(_toDoList);
     final file = await _getFile();
     return file.writeAsString(data);
-
   }
 
   Future<String?> _readData() async {
-    try{
+    try {
       final file = await _getFile();
       return file.readAsString();
-    }catch(e){
+    } catch (e) {
       return null;
     }
   }
-
-
 }
-
