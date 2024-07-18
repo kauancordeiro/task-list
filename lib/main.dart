2d38bf38f640df6e017e6a3014344f5a38ff2954
Kauan <cvitorcordeiro@gmail.com>
2024-07-18 20:28:49 -0300

    add undo snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,9 @@ class _HomeState extends State<Home> {
 
   final _toDoController = TextEditingController();
 
+  Map<String, dynamic>? _lastRemoved;
+  int? _lastRemovedPos;
+
   @override
   void initState() {
     super.initState();
@@ -91,49 +94,60 @@ class _HomeState extends State<Home> {
 
   Widget buildItem(context, index) {
     return Dismissible(
-      key: Key(DateTime.now().millisecond.toString()),
+      key: Key(DateTime
+          .now()
+          .millisecond
+          .toString()),
       background: Container(
         color: Colors.red,
         child: Align(
           alignment: Alignment(-0.9, 0.0),
-          child: Icon(Icons.delete, color: Colors.white,
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
           ),
         ),
       ),
       direction: DismissDirection.startToEnd,
       child: CheckboxListTile(
-      title: Text(_toDoList[index]["title"]),
-      value: _toDoList[index]["ok"],
-      secondary: CircleAvatar(
-        child: Icon(
-            _toDoList[index]["ok"] ? Icons.check : Icons.error),
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
+        ),
+        onChanged: (bool? isChecked) {
+          setState(() {
+            _toDoList[index]["ok"] =
+                isChecked; // Atualiza o valor na lista quando o usuário interage
+            _saveData();
+          });
+        },
       ),
-      onChanged: (bool? isChecked) {
+      onDismissed: (direction) {
         setState(() {
-          _toDoList[index]["ok"] =
-              isChecked; // Atualiza o valor na lista quando o usuário interage
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
           _saveData();
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text("Tarefa \"${_lastRemoved!["title"]}\" removida"),
+                action: SnackBarAction(
+                  label: "Desfazer",
+                  onPressed: () {
+                    setState(() {
+                      _toDoList.insert(_lastRemovedPos!, _lastRemoved);
+                      _saveData();
+                    });
+                  },
+                ),
+                duration: Duration(seconds: 2),
+              ),
+          );
         });
       },
-    ),
     );
   }
-
-  // CheckboxListTile(
-  // title: Text(_toDoList[index]["title"]),
-  // value: _toDoList[index]["ok"],
-  // secondary: CircleAvatar(
-  // child: Icon(
-  // _toDoList[index]["ok"] ? Icons.check : Icons.error),
-  // ),
-  // onChanged: (bool? isChecked) {
-  // setState(() {
-  // _toDoList[index]["ok"] =
-  // isChecked; // Atualiza o valor na lista quando o usuário interage
-  // _saveData();
-  // });
-  // },
-  // );
 
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
